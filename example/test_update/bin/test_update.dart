f9c5ecef49c981dd6e8977eb5e61503da7f91ba3
Sahad2701 <skachhawala33@gmail.com>
2026-02-22 13:06:28 +0530

    Remove example/test_update directory
    Delete the example/test_update sample application and all associated files (source, bin, lib, test, README, CHANGELOG, analysis options, pubspec, and .gitignore). Cleans up the repository by removing this unused example.

--- example/test_update/bin/test_update.dart
+++
@@ -1,5 +0,0 @@
-import 'package:test_update/test_update.dart' as test_update;
-
-void main(List<String> arguments) {
-  print('Hello world: ${test_update.calculate()}!');
-}
--- example/test_update/test/test_update_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:test_update/test_update.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
